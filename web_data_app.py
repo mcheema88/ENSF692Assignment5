d6a99a3164fa306f5e026ffd018609324c60be7b
mcheema88 <marley.cheema1@ucalgary.ca>
2025-06-17 15:28:01 -0600

    Okay sweet everything is running well -> cam access in safari but not chrome. Going to now change the code and start with name

--- web_data_app.py
+++ web_data_app.py
@@ -1,6 +1,6 @@
 # web_data_app.py
 # June 2025
-# Modified by: STUDENT NAME
+# Modified by: Marley Cheema
 #
 # An simple program for demonstrating web applications using Flask and web scraping of data using Beautiful Soup.
 # Detailed specifications are provided via the Assignment 5 README file.
